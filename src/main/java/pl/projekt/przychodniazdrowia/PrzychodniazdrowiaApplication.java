46da12cc01cb4063739665ce878335dbaef5f6fd
kamilb13 <kamil.brzoza13@gmail.com>
2025-03-20 12:09:20 +0100

    initial commit

---
+++ src/main/java/pl/projekt/przychodniazdrowia/PrzychodniazdrowiaApplication.java
@@ -0,0 +1,13 @@
+package pl.projekt.przychodniazdrowia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzychodniazdrowiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrzychodniazdrowiaApplication.class, args);
+    }
+
+}
