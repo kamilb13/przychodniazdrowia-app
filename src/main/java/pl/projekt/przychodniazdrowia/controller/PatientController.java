98cf8f3165665f6d219329df9b3e3385abffcaaa
kamilb13 <kamil.brzoza13@gmail.com>
2025-04-02 17:58:33 +0200

    added cross origin for registration patient and patient controller

--- src/main/java/pl/projekt/przychodniazdrowia/controller/PatientController.java
+++ src/main/java/pl/projekt/przychodniazdrowia/controller/PatientController.java
@@ -2,10 +2,7 @@ package pl.projekt.przychodniazdrowia.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.projekt.przychodniazdrowia.model.HealthRecord;
 import pl.projekt.przychodniazdrowia.model.Patient;
 import pl.projekt.przychodniazdrowia.respository.HealthRecordRepository;
@@ -15,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Hidden
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class PatientController {
     private final PatientRepository patientRepository;
